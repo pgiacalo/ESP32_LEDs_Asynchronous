1ea235ddd41687d81fe46b2f2d17d17c878e0fc0
Philip Giacalone <philip_giacalone@yahoo.com>
2024-06-28 09:45:13 -0700

    Added function prototypes

--- main/led_test.c
+++ main/led_test.c
@@ -5,18 +5,21 @@
 
 #define TAG "LED_TEST"
 
+// Function prototypes
+void setupLEDs();
+void ledTestTask(void *pvParameters);
+
 void app_main() {
-    
+    // Create the LED control task
+    xTaskCreate(controlLEDs, "LED Control Task", 1024, NULL, 1, NULL);
+
     setupLEDs();
 
-    // Create the LED control task
-    xTaskCreate(controlLEDs, "LED Control Task", 1024, NULL, 1, NULL);    
-
-    xTaskCreate(otherTask, "Other Task", 1024, NULL, 1, NULL);
-
+    // Create the LED test task
+    xTaskCreate(ledTestTask, "LED Test Task", 2048, NULL, 1, NULL); 
 }
 
-void setupLEDs(){
+void setupLEDs() {
     // Initialize the LEDs
     initializeLEDs();
     // Set custom intervals and brightness levels
@@ -25,14 +28,12 @@ void setupLEDs(){
     setBlueLEDBrightness(255);  // Example: set blue LED brightness to 255
 }
 
-void otherTask(void *pvParameters){
-
+void ledTestTask(void *pvParameters) {
     while (true) {
         // Example sequences to test LED behaviors and colors
         ESP_LOGI(TAG, "Test: LEDs Off");
         ledBehavior = LED_BEHAVIOR_OFF;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
 
@@ -41,21 +42,18 @@ void otherTask(void *pvParameters){
         ledColor = LED_COLOR_RED;
         ledBehavior = LED_BEHAVIOR_ON;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED On Green");
         ledColor = LED_COLOR_GREEN;
         ledBehavior = LED_BEHAVIOR_ON;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED On Blue");
         ledColor = LED_COLOR_BLUE;
         ledBehavior = LED_BEHAVIOR_ON;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         ledBehavior = LED_BEHAVIOR_OFF;
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
 
@@ -64,21 +62,18 @@ void otherTask(void *pvParameters){
         ledColor = LED_COLOR_RED;
         ledBehavior = LED_BEHAVIOR_SLOW_BLINK;
         vTaskDelay(3000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED Slow Blink Green");
         ledColor = LED_COLOR_GREEN;
         ledBehavior = LED_BEHAVIOR_SLOW_BLINK;
         vTaskDelay(3000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED Slow Blink Blue");
         ledColor = LED_COLOR_BLUE;
         ledBehavior = LED_BEHAVIOR_SLOW_BLINK;
         vTaskDelay(3000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         ledBehavior = LED_BEHAVIOR_OFF;
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
 
@@ -87,21 +82,18 @@ void otherTask(void *pvParameters){
         ledColor = LED_COLOR_RED;
         ledBehavior = LED_BEHAVIOR_BLINK;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED Blink Green");
         ledColor = LED_COLOR_GREEN;
         ledBehavior = LED_BEHAVIOR_BLINK;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED Blink Blue");
         ledColor = LED_COLOR_BLUE;
         ledBehavior = LED_BEHAVIOR_BLINK;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         ledBehavior = LED_BEHAVIOR_OFF;
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
 
@@ -110,21 +102,18 @@ void otherTask(void *pvParameters){
         ledColor = LED_COLOR_RED;
         ledBehavior = LED_BEHAVIOR_FAST_BLINK;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED Fast Blink Green");
         ledColor = LED_COLOR_GREEN;
         ledBehavior = LED_BEHAVIOR_FAST_BLINK;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
         
         ESP_LOGI(TAG, "Test: LED Fast Blink Blue");
         ledColor = LED_COLOR_BLUE;
         ledBehavior = LED_BEHAVIOR_FAST_BLINK;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         ledBehavior = LED_BEHAVIOR_OFF;
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
 
@@ -132,10 +121,11 @@ void otherTask(void *pvParameters){
         ESP_LOGI(TAG, "Test: LED Cycle");
         ledBehavior = LED_BEHAVIOR_CYCLE;
         vTaskDelay(6000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "");
         ledBehavior = LED_BEHAVIOR_OFF;
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 second delay
 
         // Repeat the sequence
     }
 }
+
+
